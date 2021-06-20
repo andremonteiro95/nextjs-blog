c6e6e1b7c5f1c2c652c1c536223c5f703afb0851
andremonteiro95 <andremonteiro95@gmail.com>
2021-06-20 16:02:43 +0100

    fix page title

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -10,7 +10,7 @@ export default function PostPage(props) {
     <>
       <Head>
         <title>
-          {post.title} &dash; {BLOG_TITLE}
+          {post.title} – {BLOG_TITLE}
         </title>
         <meta name="description" content={post.description} />
         <link rel="icon" href="/favicon.ico" />
