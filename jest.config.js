b87b56f191debf3b6994991e04dcc928f0c771e0
andremonteiro95 <andremonteiro95@gmail.com>
2021-06-23 15:15:58 +0100

    configure coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   testPathIgnorePatterns: ['./.next/', './node_modules/'],
   testEnvironment: 'jsdom',
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  coverageDirectory: './coverage',
 };
